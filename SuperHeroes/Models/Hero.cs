537601ab1ac98942b7f5f28519e571e44c3a72cf
Dean Bourgeois <DeanBourgeois@wi.rr.com>
2019-03-01 11:00:25 -0600

    Created project, created Hero class with its member variables

---
+++ SuperHeroes/Models/Hero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroes.Models
+{
+    public class Hero
+    {
+        int ID;
+        string name;
+        string alterEgo;
+        string primaryAbility;
+        string secondaryAbility;
+        string catchPhrase;
+    }
+}
