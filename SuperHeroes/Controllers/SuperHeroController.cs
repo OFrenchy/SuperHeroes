cc0f488edf7fe2caa95cd4f0155c799e69a263ce
Dean Bourgeois <DeanBourgeois@wi.rr.com>
2019-03-03 16:26:24 -0600

    Added code to SuperHeroController, all return View lines

--- SuperHeroes/Controllers/SuperHeroController.cs
+++ SuperHeroes/Controllers/SuperHeroController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SuperHeroes.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,16 +9,23 @@ namespace SuperHeroes.Controllers
 {
     public class SuperHeroController : Controller
     {
+        ApplicationDbContext db;
+
+        public SuperHeroController()
+        {
+            db = new ApplicationDbContext();
+        }
+
         // GET: SuperHero
         public ActionResult Index()
         {
-            return View();
+            return View(db.SuperHeroes.OrderBy(o => o.Name).ToList());
         }
 
         // GET: SuperHero/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            return View(db.SuperHeroes.Find(id));// .Select(s => s.ID == id).First());
         }
 
         // GET: SuperHero/Create
@@ -28,61 +36,82 @@ namespace SuperHeroes.Controllers
 
         // POST: SuperHero/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(SuperHero superHero)
         {
             try
             {
                 // TODO: Add insert logic here
-
+                db.SuperHeroes.Add(superHero);
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return View(superHero);
             }
         }
 
         // GET: SuperHero/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            //db.SuperHeroes.Select(s => s.ID == id).First();
+            return View(db.SuperHeroes.Find(id));//.Select(s => s.ID == id).First());
         }
 
         // POST: SuperHero/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, SuperHero superHero)//, FormCollection collection)
         {
             try
             {
-                // TODO: Add update logic here
+                SuperHero thisSuperHero = db.SuperHeroes.Find(id);// .Where(s => s.ID == id).First();
 
+                // why doesn't this work???
+                //foreach (var field in thisSuperHero.GetType().GetFields())
+                //foreach (var field in thisSuperHero.GetType().GetFields())
+                //{
+                //    //string fieldName = field.Name;
+                //    //var value = field.GetValue(thisSuperHero);
+                //    thisSuperHero.GetType().GetField(field.Name).SetValue(thisSuperHero, superHero.GetType().GetField(field.Name).GetValue(superHero));
+                //}
+
+                if (superHero.Name != null && superHero.Name != "") thisSuperHero.Name = superHero.Name;
+                if (superHero.AlterEgo != null && superHero.AlterEgo != "") thisSuperHero.AlterEgo = superHero.AlterEgo;
+                if (superHero.CatchPhrase != null && superHero.CatchPhrase != "") thisSuperHero.CatchPhrase = superHero.CatchPhrase;
+                if (superHero.PrimaryAbility != null && superHero.PrimaryAbility != "") thisSuperHero.PrimaryAbility = superHero.PrimaryAbility;
+                if (superHero.SecondaryAbility != null && superHero.SecondaryAbility != "") thisSuperHero.SecondaryAbility = superHero.SecondaryAbility;
+                thisSuperHero.Favorite = superHero.Favorite;
+                if (superHero.ImageFile != null && superHero.ImageFile != "") thisSuperHero.ImageFile = superHero.ImageFile;
+
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return View(superHero);
             }
         }
 
         // GET: SuperHero/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            SuperHero superHero = db.SuperHeroes.Find(id);
+            return View(superHero);
         }
 
         // POST: SuperHero/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int id, SuperHero superHero)
         {
             try
             {
-                // TODO: Add delete logic here
-
+                db.SuperHeroes.Remove(db.SuperHeroes.Find(id));
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return View(superHero);
             }
         }
     }
